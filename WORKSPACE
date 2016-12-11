e4219ad0d57c1509db6ff8df88b30efe00757b5f
willard379 <willard379@gmail.com>
2016-12-11 22:07:58 +0900

    Mavenリポジトリのcommons-lang3の依存性を追加

---
+++ WORKSPACE
@@ -0,0 +1,4 @@
+maven_jar(
+	name = "commons_lang3",
+	artifact = "org.apache.commons:commons-lang3:3.5",
+)
--- src/main/java/willard379/bazel/sample/SystemPropertiesViewer.java
+++ src/main/java/willard379/bazel/sample/SystemPropertiesViewer.java
@@ -1,12 +1,14 @@
 package willard379.bazel.sample;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 public class SystemPropertiesViewer {
 
 	public static void main(String[] args) {
 		System.getProperties()
 			.entrySet()
 			.stream()
-			.map(e -> String.format("%s : %s", e.getKey(), e.getValue()))
+			.map(e -> String.format("%s : %s", e.getKey(), StringEscapeUtils.escapeJava((String) e.getValue())))
 			.sorted()
 			.forEach(System.out::println);
 	}
